b27ab00ecb46a19000380a0f5e42c1c52615a0f9
christopher lopez <christopherwlopez89@gmail.com>
2023-07-31 17:51:10 -0700

    got server running

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const { config } = require("dotenv");
-const {start} =require("./src/server");
+'use strict';
 
-config();
+const express = require("express");
+const axios = require("axios");
+const app = express();
 
-start(3000);
+const YELP_API_URL = "https://api.yelp.com/v3/businesses/search";
+const YELP_API_KEY= process.env.API_KEY;
+app.use(express.json());
+
+app.get("/api/yelp", async(req,res)=>{
+    try{
+        const { term, location, limit }=req.query;
+        const url = `${YELP_API_URL}?term=${term}&location=${location}&limit=${limit}`;
+        const headers ={
+            Authorization: `Bearer ${YELP_API_KEY}`,
+        };
+
+        const response = await axios.get(url, { headers });
+        res.json(response.data);
+    } catch(error) {
+        console.error("Error with Data", error);
+        res.status(500).json({error:"Error fetching data"});
+    }
+});
+
+    const port = 3000;
+    app.listen(port,()=>console.log(`listening on ${port}`))
